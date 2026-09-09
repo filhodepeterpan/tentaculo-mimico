2f3e27dfc3553a2bd3348afc5667fd3eafd2b471
Douglas Soares <douglassoares.cinema@gmail.com>
2026-09-09 18:55:01 -0300

    feat: criacao da aplicacao

---
+++ src/main/java/br/com/tentaculomimico/TentaculoMimicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.tentaculomimico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TentaculoMimicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TentaculoMimicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tentaculomimico/TentaculoMimicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tentaculomimico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TentaculoMimicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
